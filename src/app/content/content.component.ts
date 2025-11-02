29cfdc49614f089ffd59d237c007e208b178fd47
David Wilke <david-wilke@live.com>
2025-11-02 19:43:09 +0100

    change component order

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -3,11 +3,13 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ReviewsComponent } from "./reviews/reviews.component";
+import { ContactFormComponent } from "../contact-form/contact-form.component";
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [AboutMeComponent, AboveTheFoldComponent, ProjectsComponent, SkillsComponent],
+  imports: [AboutMeComponent, AboveTheFoldComponent, ProjectsComponent, SkillsComponent, ReviewsComponent, ContactFormComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.scss'
 })
