f082ee1d6db2baab3f03dc2dfc8510a9c2303fb4
David Wilke <david-wilke@live.com>
2025-11-04 03:16:21 +0100

    styling projects component

--- src/app/content/projects/projects.component.ts
+++ src/app/content/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ApplicationComponent } from "./application/application.component";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [ApplicationComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
