4feea73a9be6f80dca54b6a555c11490f43c5570
David Wilke <david-wilke@live.com>
2025-11-04 03:15:30 +0100

    add application component

---
+++ src/app/content/projects/application/application.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application',
+  standalone: true,
+  imports: [],
+  templateUrl: './application.component.html',
+  styleUrl: './application.component.scss'
+})
+export class ApplicationComponent {
+
+// Hier jetzt ein array erstellen welches details links und Bilder zu den Projekten beinhaltet
+
+
+}
