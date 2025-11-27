18742d4122d13b657dd2e4b25c7e6bda4d89edc0
David Wilke <david-wilke@live.com>
2025-11-27 21:08:15 +0100

    add review template and style

--- src/app/content/reviews/reviews.component.ts
+++ src/app/content/reviews/reviews.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reviews',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.scss'
 })
 export class ReviewsComponent {
 
+  current: number = 0;
+
+
+  reviews = [
+    {
+      id: 1,
+      name: 'John Doe',
+      text: 'Great service and friendly staff. Highly recommend!',
+      image: '../../../assets/icons/placeholder.png'
+    },
+    {
+      id: 2,
+      name: 'Martin Mustermann',
+      text: 'Great service and friendly staff. Highly recommend!',
+      image: '../../../assets/icons/placeholder.png'
+    },
+    {
+      id: 1,
+      name: 'John Doe',
+      text: 'Great service and friendly staff. Highly recommend!',
+      image: '../../../assets/icons/placeholder.png'
+    },
+    {
+      id: 2,
+      name: 'Martin Mustermann',
+      text: 'Great service and friendly staff. Highly recommend!',
+      image: '../../../assets/icons/placeholder.png'
+    }
+  ];
+
+
+  next() {
+    this.current = (this.current + 1) % this.reviews.length;
+  }
+
+  prev() {
+    this.current = (this.current - 1 + this.reviews.length) % this.reviews.length;
+  }
 }
