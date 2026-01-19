fd19ce2078703edf055f882145c1592983fde7be
David Wilke <david-wilke@live.com>
2026-01-19 08:43:35 +0100

    Outsourcing texts, converting them to a translation service

--- src/app/content/about-me/about-me.component.ts
+++ src/app/content/about-me/about-me.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { TranslateDirective } from '@ngx-translate/core';
+
+
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [TranslateDirective],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
